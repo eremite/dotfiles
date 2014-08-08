331f3750ba5f860264b6ba7ebd8795962b1a740b
Daniel Esplin <daniel@custombit.com>
2014-08-08 16:32:37 -0600

    devbox_init.sh: git init dotfiles to get all the git hook goodness.

--- devbox_init.sh
+++ devbox_init.sh
@@ -9,6 +9,6 @@ cp /personal/id_rsa* ~/.ssh
 cp /personal/.netrc ~
 
 # Update and trigger checkout callbacks
-cd ~/dotfiles; git checkout master; git pull
+cd ~/dotfiles; git init; git checkout master; git pull
 cd ~/done; git checkout master; git pull
 cd ~/docker_rails_app; git checkout master; git pull
