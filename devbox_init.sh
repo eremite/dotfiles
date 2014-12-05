9ec2ebb2df9f68c8ce2452f7333b11f2b5869824
Daniel Esplin <daniel@custombit.com>
2014-12-05 11:33:48 -0700

    Devbox: No need to fix volume permission anymore.
    Because we're being smarter when creating the data volume.

--- devbox_init.sh
+++ devbox_init.sh
@@ -3,9 +3,6 @@
 # So I can docker without sudo
 sudo chmod o+rw /var/run/docker.sock
 
-# Fix permissions of attached volumes
-sudo chown -R dev:dev $DATA
-
 # Update and trigger checkout callbacks
 cd ~/dotfiles; git init; git checkout master; git pull
 
