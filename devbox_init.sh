8d4fa1de40ac7686d82275cdfe73817609615b89
Daniel Esplin <daniel@custombit.com>
2014-12-05 09:58:00 -0700

    devbox init: Improve setting up ssh config.

--- devbox_init.sh
+++ devbox_init.sh
@@ -6,16 +6,14 @@ sudo chmod o+rw /var/run/docker.sock
 # Fix permissions of attached volumes
 sudo chown -R dev:dev $DATA
 
-# Authentication permissions
-cp $META/id_rsa* ~/.ssh
-cp $META/.netrc ~
-cd ~/.ssh; rm config; ln -s $META/ssh_config config
-
 # Update and trigger checkout callbacks
 cd ~/dotfiles; git init; git checkout master; git pull
 cd ~/done; git checkout master; git pull
 
 cd ~/bin; ln -s ~/done/done.thor; ln -s $DATA/docker_rails_app/docker_rails_app.sh
+
+# Set up ssh config
+cd ~/.ssh; ~/.git_template/hooks/create_symlinks
 
 cd $DATA
 tmux
