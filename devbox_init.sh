589f0a1a5c82805c7d0ac2156c58e019d685aeec
Daniel Esplin <daniel@custombit.com>
2014-12-11 11:23:03 -0700

    Use rcup to install secure keys and netrc.

--- devbox_init.sh
+++ devbox_init.sh
@@ -9,16 +9,15 @@ sudo chmod o+rw /var/run/docker.sock
 if [ ! -d "$DATA/dotfiles" ]; then
   git clone https://github.com/eremite/dotfiles.git $DATA/dotfiles
 fi
-cd; ln -s $DATA/dotfiles/rcrc .rcrc; rcup
+if [ -d "$DATA/meta/secure" ]; then
+  cp $DATA/meta/secure/netrc $DATA/dotfiles
+  cp $DATA/meta/secure/id_rsa* $DATA/dotfiles/ssh
+fi
+cd; RCRC=$DATA/dotfiles/rcrc rcup
+rm $DATA/dotfiles/netrc $DATA/dotfiles/ssh/id_rsa*
 vim +PlugInstall +qall # Install vim plugins
 # Patch app_rake_command in vim-rails to support running rake with fig.
 cd /home/dev/.vim/plugged/vim-rails; patch -p1 < $DATA/meta/dotfiles/rails_app_rake_command.patch
-
-# Set up ssh
-mkdir -p ~/.ssh; cd ~/.ssh; ~/.git_template/hooks/create_symlinks
-
-# Set up and trigger hooks on checkout
-cd $DATA/dotfiles; git init; git checkout master
 
 # Start tmux
 cd $DATA
