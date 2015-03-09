e098152f582aa9477a7e0db3b11c2988937c1f52
Daniel Esplin <daniel@custombit.com>
2015-03-09 11:44:15 -0600

    Add a script for backing up and restoring meta data.

---
+++ meta_backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install awscli with apt-get
+# sudo apt-get -y install python
+# curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+# sudo python get-pip.py
+# sudo pip install awscli
+
+export AWS_ACCESS_KEY_ID=EXAMPLE
+export AWS_SECRET_ACCESS_KEY=EXAMPLE
+
+filename=meta.$(date +"%Y.%m.%d").tar.gz
+s3_bucket=daniel-devbox
+
+# Backup
+tar -C $META --exclude='**/tmp' -c -f - . | gzip > $filename
+gpg -c $filename
+aws s3 cp ${filename}.gpg s3://$s3_bucket/
+
+# Restore
+cd /data
+mkdir meta
+cd meta
+aws s3 cp s3://$s3_bucket/${filename}.gpg .
+gpg -d ${filename}.gpg > $filename
+tar -xzf $filename
