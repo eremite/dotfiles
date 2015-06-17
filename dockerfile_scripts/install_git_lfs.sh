a20b79982afd5f5b18eaa8e1dd4d5565bfaf68e7
Daniel Esplin <daniel@custombit.com>
2015-06-17 12:55:47 -0600

    git lfs: Switch to amd64.

--- dockerfile_scripts/install_git_lfs.sh
+++ dockerfile_scripts/install_git_lfs.sh
@@ -1,8 +1,8 @@
 VERSION=0.5.1
-wget https://github.com/github/git-lfs/releases/download/v$VERSION/git-lfs-linux-386-$VERSION.tar.gz
-tar -zxf git-lfs-linux-386-$VERSION.tar.gz
+wget https://github.com/github/git-lfs/releases/download/v$VERSION/git-lfs-linux-amd64-$VERSION.tar.gz
+tar -zxf git-lfs-linux-amd64-$VERSION.tar.gz
 cp git-lfs-$VERSION/git-lfs /usr/local/bin
-rm git-lfs*
+rm -r git-lfs*
 
 echo -n "checking git lfs..."
 git lfs version | grep $VERSION > /dev/null
