7f7680198e065cb5b363dad81a53cf833b19d828
Daniel Esplin <daniel@custombit.com>
2015-06-30 16:29:54 -0600

    devbox_init: Load .dockercfg from secure.

--- devbox_init.sh
+++ devbox_init.sh
@@ -11,11 +11,12 @@ if [ ! -d "$DATA/dotfiles" ]; then
 fi
 if [ -d "$DATA/meta/secure" ]; then
   cp $DATA/meta/secure/netrc $DATA/dotfiles
+  cp $DATA/meta/secure/dockercfg $DATA/dotfiles
   cp $DATA/meta/secure/id_rsa* $DATA/dotfiles/ssh
 fi
 cd; RCRC=$DATA/dotfiles/rcrc rcup
 mkdir -p ~/.config; ln -sf $DATA/meta/secure/hub ~/.config/hub
-rm $DATA/dotfiles/netrc $DATA/dotfiles/ssh/id_rsa*
+rm $DATA/dotfiles/netrc $DATA/dotfiles/dockercfg $DATA/dotfiles/ssh/id_rsa*
 if [ -d "$DATA/meta/dotfiles/plugged" ]; then
   mkdir -p $HOME/.vim
   cd $HOME/.vim
