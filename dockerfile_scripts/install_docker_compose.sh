01b73530ce814c074d47f7ffb7d9b3e0c2508779
Daniel Esplin <daniel@custombit.com>
2015-09-11 10:34:43 -0600

    Dockerfile: Upgrade docker-compose to 1.4.0

--- dockerfile_scripts/install_docker_compose.sh
+++ dockerfile_scripts/install_docker_compose.sh
@@ -1,4 +1,4 @@
-VERSION=1.3.1
+VERSION=1.4.0
 curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
