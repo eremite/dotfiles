e038a49ceb910df5c02b9b10e1ab0fc346ddcb14
Daniel Esplin <daniel@custombit.com>
2016-02-25 18:34:36 +0000

    meta_setup: copy docker_overrides over from meta if it exists.

--- meta_setup.sh
+++ meta_setup.sh
@@ -5,6 +5,9 @@ set -x
 app=${PWD##*/}
 mkdir -p $META/$app/tmp
 mkdir -p $META/$app/symlinks/.git
+if [ -e $META/$app/docker_overrides.env ]; then
+  cp $META/$app/docker_overrides.env .docker_overrides.env
+fi
 touch $META/$app/symlinks/notes.md
 mv .git/config $META/$app/symlinks/.git
 git checkout master
