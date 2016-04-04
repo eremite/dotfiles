a7f74095288b61bdce4ff979df1868669e48db52
Daniel Esplin <daniel@custombit.com>
2016-04-04 10:04:37 -0600

    Make changes to scripts recommended by shellcheck.

--- bin/meta_backup.sh
+++ bin/meta_backup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# let g:syntastic_sh_sh_exe = 'docker run --rm -v $(pwd):/source cromo/shellcheck shellcheck'
-# let g:syntastic_sh_sh_args = ''
-# let g:syntastic_sh_sh_fname = ''
-# let g:syntastic_sh_sh_post_args = ''
-# let g:syntastic_sh_sh_tail = ''
-
 # Install awscli with apt-get
 # sudo apt-get -y install python
 # curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
@@ -19,16 +13,16 @@ filename=meta.$(date +"%Y.%m.%d").tar.gz
 s3_bucket=daniel-devbox
 
 # Backup
-tar -C $META --exclude='**/tmp' -c -f - . | gzip > $filename
-gpg -c $filename
-aws s3 cp ${filename}.gpg s3://$s3_bucket/
+tar -C $META --exclude='**/tmp' -c -f - . | gzip > "$filename"
+gpg -c "$filename"
+aws s3 cp "${filename}.gpg" "s3://$s3_bucket/"
 
 # Restore
 cd
-mkdir meta
-cd meta
+mkdir "meta"
+cd "meta"
 sudo chown -R dev:dev .
-aws s3 cp s3://$s3_bucket/${filename}.gpg .
-gpg -d ${filename}.gpg > $filename
-tar -xzf $filename
+aws s3 cp "s3://$s3_bucket/${filename}.gpg" .
+gpg -d "${filename}.gpg" > "$filename"
+tar -xzf "$filename"
 rm meta*
--- bin/meta_setup.sh
+++ bin/meta_setup.sh
@@ -3,11 +3,11 @@
 set -x
 
 app=${PWD##*/}
-mkdir -p $META/$app/tmp
-mkdir -p $META/$app/symlinks/.git
-:if [ -e $META/$app/docker_overrides.env ]; then
-  cp $META/$app/docker_overrides.env .docker_overrides.env
+mkdir -p "$META/$app/tmp"
+mkdir -p "$META/$app/symlinks/.git"
+if [ -e "$META/$app/docker_overrides.env" ]; then
+  cp "$META/$app/docker_overrides.env" .docker_overrides.env
 fi
-touch $META/$app/symlinks/notes.md
-mv .git/config $META/$app/symlinks/.git
+touch "$META/$app/symlinks/notes.md"
+mv .git/config "$META/$app/symlinks/.git"
 git checkout master
