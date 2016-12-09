89014991f73018edb05cd8bf06c3b58d7cd6fdb8
Daniel Esplin <daniel@custombit.com>
2016-12-09 09:47:32 -0700

    devbox_update: Add some echos to organize the code.

--- bin/devbox_update.sh
+++ bin/devbox_update.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -x
+
+echo "Updating OS packages"
 sudo yum -y update
+
+echo "Update heroku"
 heroku update
+
+echo "Update vim plugins"
 cd "$HOME/.vim/plugged/vim-rails"
 git stash
 $DATA/neovim/bin/nvim -c 'PlugUpgrade|PlugUpdate'
