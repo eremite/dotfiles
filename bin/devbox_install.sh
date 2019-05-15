420a51784c7f1f8752a994893170dc86cb2aec9a
Daniel Esplin <daniel@storageunitsoftware.com>
2019-05-15 13:49:23 -0600

    devbox_install: Fix install ssh keys.

--- bin/devbox_install.sh
+++ bin/devbox_install.sh
@@ -18,14 +18,17 @@ sudo apt-get install -y \
   tmux \
   wget
 
-# Install SSH key
-nvim ~/.ssh/id_rsa
+# Install private SSH key
+mkdir ~/.ssh
+vim ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
 
 # Install rcup: https://github.com/thoughtbot/rcm
+
 git clone git://github.com:eremite/dotfiles.git
 cd; RCRC=/home/eremite/dotfiles/rcrc rcup -f
 
-# https://docs.docker.com/install/linux/docker-ce/debian/
+# Install Docker (https://docs.docker.com/install/linux/docker-ce/debian/)
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
