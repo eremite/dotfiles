fb78aa5dc48286e4a6009b6670120b091e78b889
Daniel Esplin <daniel@storageunitsoftware.com>
2021-09-28 11:39:33 -0600

    devbox install: The docker-compose install process has changed.

--- bin/devbox_install.sh
+++ bin/devbox_install.sh
@@ -28,20 +28,25 @@ chmod 400 ~/.ssh/id_rsa
 git clone git@github.com:eremite/dotfiles.git
 cd; RCRC=$HOME/dotfiles/rcrc rcup -f
 
+vim -c 'PlugInstall'
+
 # Install Docker (https://docs.docker.com/install/linux/docker-ce/debian/)
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker "$USER"
-reboot
-# docker login
+sudo reboot
 
 # https://docs.docker.com/compose/install/
+DOCKER_COMPOSE_VERSION=v2.0.0
+curl -SL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-linux-amd64" -o "$HOME/.docker/cli-plugins/docker-compose"
+chmod +x !$
 
-# https://ngrok.com/download
-# wget $FILE
-# unzip $FILE
-# sudo mv ngrok /usr/local/bin
+# Navigate to project_notes in the Files app and "Share with Linux"
+PROJECT=project
+cd
+git clone "git@github.com:$OWNER/$PROJECT.git"
+cp "/mnt/chromeos/GoogleDrive/MyDrive/project_notes/$PROJECT.md" "$HOME/$PROJECT/notes.md"
+cp "/mnt/chromeos/GoogleDrive/MyDrive/project_notes/$PROJECT.rb" "$HOME/$PROJECT/notes.rb"
+cp "/mnt/chromeos/GoogleDrive/MyDrive/project_notes/$PROJECT.yml" "$HOME/$PROJECT/docker-compose.yml"
 
 aws configure
-
-vim -c 'PlugInstall'
