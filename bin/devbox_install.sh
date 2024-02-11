4b236fba1f1cff5df0f8b7c9eac327ece16f0ad0
eremite <daniel@danielesplin.org>
2024-02-11 15:22:47 -0700

    devbox_install: Update and fix to work with default Chrome OS Linux.

--- bin/devbox_install.sh
+++ bin/devbox_install.sh
@@ -2,32 +2,27 @@
 
 set -x
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
 
-sudo apt-get install -y \
+sudo apt install -y \
   apt-transport-https \
   ca-certificates \
-  ctags \
+  universal-ctags \
   curl \
   dnsutils \
   git \
   gnupg2 \
   libterm-readkey-perl \
+  rcm \
   software-properties-common \
   wget
-
-# Install Chrome Extensions
-# LastPass: https://chrome.google.com/webstore/detail/lastpass-free-password-ma/hdokiejnpimakedhajhdlcegeplioahd?hl=en
-# Vimium: https://chrome.google.com/webstore/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb?hl=en
 
 # Install private SSH key
 mkdir ~/.ssh
 vim ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
-
-sudo apt install rcm # Install rcup: https://github.com/thoughtbot/rcm
 
 git clone git@github.com:eremite/dotfiles.git
 cd; RCRC=$HOME/dotfiles/rcrc rcup -f
@@ -39,11 +34,6 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker "$USER"
 sudo reboot
-
-# Install Docker Compose (https://docs.docker.com/compose/cli-command/#install-on-linux)
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.9.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
 
 # Navigate to project_notes in the Files app and "Share with Linux"
 PROJECT=project
